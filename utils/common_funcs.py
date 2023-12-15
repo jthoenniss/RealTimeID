4b3badab59c6ae8d23048c3989e4dbdd5fd10846
Julian Thoenniss <julthoe@gmail.com>
2023-12-15 15:41:20 +0100

    make loop through h_vals more concise by denfining external utility functions

--- utils/common_funcs.py
+++ utils/common_funcs.py
@@ -14,3 +14,37 @@ def create_numpy_arrays(D):
     dtypes = [float, int, int, float]
 
     return tuple(np.array(arr, dtype=dtype) for arr, dtype in zip(zip(*data), dtypes))
+
+
+def check_error_condition(eps_current, eps_previous):
+    """
+    Check the error condition to determine if the iteration should be stopped.
+
+    Parameters:
+    - eps_current (float): Current error value.
+    - eps_previous (float): Previous error values.
+
+    Returns:
+    - bool: True if the condition is met, indicating the iteration should be stopped; otherwise, False.
+    """
+    if eps_current > eps_previous or eps_current < 1.0e-15:
+        print(
+            f"Either the error does not shrink with decreasing h or the machine precision error was reached. "
+            f"Error = {eps_current}. Stopping the iteration."
+        )
+        return True
+    return False
+
+
+
+def update_grid_parameters(params, h):
+    """
+    Update grid parameters in the given dictionary.
+
+    Parameters:
+    - params (dict): Dictionary containing grid parameters.
+    - h (float): New value for the discretization parameter 'h'.
+    """
+    params["h"] = h
+    params["m"] = int(10.0 / h)
+    params["n"] = int(5.0 / h)
