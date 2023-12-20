5fcb8aa0a904e6a45b4d2cc3f12d29aed4b34b60
Julian Thoenniss <julthoe@gmail.com>
2023-12-20 17:23:27 +0100

    updated test files

--- tests/test_KernelMatrix.py
+++ tests/test_KernelMatrix.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 from src.kernel_matrix.kernel_matrix import KernelMatrix
-
+from src.utils import common_funcs as cf
 
 class TestKernelMatrix(unittest.TestCase):
     def setUp(self):
@@ -25,6 +25,32 @@ class TestKernelMatrix(unittest.TestCase):
 
         for key in KernelMatrix_keys_required:#check that all attributes exist.
             self.assertTrue(key in KernelMatrix_keys, f"Key {key} not found.")
+
+    def test_initialization(self):
+        # Test initialization of RtKernel
+        self.assertEqual(self.K.m, 10)
+        self.assertEqual(self.K.n, 5)
+        self.assertEqual(self.K.beta, 1.0)
+        self.assertEqual(self.K.N_max, 10)
+        self.assertEqual(self.K.delta_t, 0.1)
+        self.assertTrue(np.array_equal(self.K.times, cf.set_time_grid(N_max=10, delta_t=0.1)))
+        self.assertEqual(self.K.h, 0.2)
+        self.assertEqual(self.K.phi, np.pi / 4)
+
+    def test_create_kernel(self):
+        # Test the kernel creation method
+        fine_grid, k_values = self.K.fine_grid, self.K.k_values
+        k_values_check = np.arange(-self.K.n, self.K.m + 1)
+        fine_grid_check = np.exp(
+            self.K.h * k_values_check - np.exp(-self.K.h * k_values_check)
+        )
+        self.assertTrue(np.array_equal(k_values, k_values_check))
+        self.assertTrue(np.array_equal(fine_grid, fine_grid_check))
+
+    def test_kernel_matrix_shape(self):
+        K = self.K.kernel
+        expected_shape = (len(self.K.times), self.K.m + self.K.n + 1)
+        self.assertEqual(K.shape, expected_shape)
 
 if __name__ == "__main__":
     unittest.main()
