f8de2f7f4de0d9bf4a10fc32479e454e913f5cff
Julian Thoenniss <julthoe@gmail.com>
2023-12-20 15:08:54 +0100

    lightened the initialization routines of DecompKernel and DlrKernel

--- src/dlr_kernel/dlr_kernel.py
+++ src/dlr_kernel/dlr_kernel.py
@@ -27,34 +27,54 @@ class DecompKernel(KernelMatrix):
         """
 
         if args:
-            if isinstance(args[0], DiscrError):
+            if args[0] is None:
+                self._initialize_with_defaults()
+            elif isinstance(args[0], DiscrError):
                 self._initialize_from_DiscrError(args[0])
             else:
                 raise ValueError(
-                    f"No known method to initiliaze DecompKernel from object of type {type(args[0].__name__)}."
+                    f"No known method to initialize DecompKernel from object of type {type(args[0].__name__)}."
                 )
 
         elif kwargs:
-            # Check that all required parameters (defined in RtDlr.REQUIRED_PARAMS) are present
-            ParameterValidator.validate_required_params(
-                kwargs, DecompKernel.REQUIRED_PARAMS
-            )
-            # read error and initialize.
-            eps = kwargs.pop("eps", None)  # Extract 'eps' and remove it from kwargs
-            ParameterValidator.validate_eps(eps)
-            self.eps = eps
-            # initialize base class
-            super().__init__(**kwargs)
+            self._initialize_from_kwargs(kwargs)
+        
         else:
             raise ValueError("Arguments required for initialization not provided.")
 
-        # Perform SVD and ID
-        self._perform_decompositions()
 
-        # compute coarse ID grid
-        self.coarse_grid = self._compute_coarse_grid()
 
     # _______________End Initialization Routine___________________________
+    def _initialize_with_defaults(self) -> None:
+        """
+        Private method to initialize with default values when no arguments are provided or argument is None.
+        In this case, the initializer of the parent class Rt_Kernel is not called.
+        """
+        # Default initialization of integer, float, and array attributes
+        integer_attributes = [
+            "m",
+            "n",
+            "N_max",
+            "nbr_sv_above_eps",
+            "ID_rank",
+        ]
+        float_attributes = ["beta", "delta_t", "eps", "h", "phi", "singular_values"]
+        array_attributes = [
+            "times",
+            "fine_grid",
+            "kernel",
+            "idx",
+            "proj",
+            "coarse_grid",
+        ]
+
+        for member in integer_attributes:
+            setattr(self, member, 0)
+        for member in float_attributes:
+            setattr(self, member, 0.0)
+        for member in array_attributes:
+            setattr(self, member, np.array([]))
+
 
     def _initialize_from_DiscrError(self, D: DiscrError) -> None:
         """
@@ -72,12 +92,32 @@ class DecompKernel(KernelMatrix):
         for key, value in params_KernelMatrix.items():
             setattr(self, key, value)
 
+        # Perform SVD and ID and set coarse grid
+        self._perform_decompositions()
+
+    def _initialize_from_kwargs(self,kwargs) -> None:
+        # Check that all required parameters (defined in RtDlr.REQUIRED_PARAMS) are present
+        ParameterValidator.validate_required_params(
+            kwargs, DecompKernel.REQUIRED_PARAMS
+        )
+        # read error and initialize.
+        eps = kwargs.pop("eps", None)  # Extract 'eps' and remove it from kwargs
+        ParameterValidator.validate_eps(eps)
+        self.eps = eps
+        # initialize base class
+        super().__init__(**kwargs)
+        # Perform SVD and ID and set coarse grid
+        self._perform_decompositions()
+        
+
     def _perform_decompositions(self):
         """
         Performs SVD and ID on the kernel matrix.
         """
         self.nbr_sv_above_eps, self.singular_values = self.perform_svd()
         self.ID_rank, self.idx, self.proj = self.perform_ID()
+        # compute coarse ID grid
+        self.coarse_grid = self._compute_coarse_grid()
 
     def perform_svd(self, eps=None):
         """
@@ -119,10 +159,6 @@ class DlrKernel(DecompKernel):
     """
     Class providing an interface to class DecompKernel, extending its functionalities and providing access to effective quantities.
     """
-
-    # Parameters required for the initialization of the class
-    REQUIRED_PARAMS = ["m", "n", "beta", "N_max", "delta_t", "eps", "h", "phi"]
-
     def __init__(self, *args, **kwargs):
         """
         Parameters are passed
@@ -134,83 +170,8 @@ class DlrKernel(DecompKernel):
         - eps (float): Error threshold for SVD and ID (see DecompKernel).
         """
 
-        if args:
-            self._initialize_from_args(args)
-        elif kwargs:
-            self._validate_and_initialize(kwargs)
-        else:
-            self._initialize_with_defaults()
+        super().__init__(*args,**kwargs)
 
-    # Function definitions used in initilization:
-    def _initialize_from_args(self, args):
-        """
-        Initializes the object from the first argument in 'args'.
-
-        The method supports this type of initialization in two ways:
-        1. With defaults: Initializes with default values if the first argument is None.
-        2. From an object: Uses attributes of the first argument.
-        """
-
-        if args[0] is None:
-            self._initialize_with_defaults()
-        else:
-            self._initialize_from_object(args[0])
-
-    def _initialize_from_object(self, obj):
-        """
-        Initialize the class from an object 'obj' (e.g. of type DiscrError) from which we extract the attributes.
-        """
-        if isinstance(obj, DiscrError):
-            super().__init__(obj)
-
-        else:
-            init_params = {
-                param: getattr(obj, param)
-                for param in DlrKernel.REQUIRED_PARAMS
-                if hasattr(obj, param)
-            }  # check for each required attribute in RtDlr.REQUIRED_PARAMS
-
-            # check if all required attributes are there, if yes, initialize, otherwise error will be thrown
-            self._validate_and_initialize(init_params)
-
-    def _validate_and_initialize(self, params):
-        """
-        Validate parameters (i.e. check if all required arguments are present) and initialize the class.
-        """
-        ParameterValidator.validate_required_params(params, DlrKernel.REQUIRED_PARAMS)
-        super().__init__(**params)
-
-    def _initialize_with_defaults(self):
-        """
-        Private method to initialize with default values when no arguments are provided or argument is None.
-        In this case, the initializer of the parent class Rt_Kernel is not called.
-        """
-        # Default initialization of integer, float, and array attributes
-        integer_attributes = [
-            "m",
-            "n",
-            "N_max",
-            "nbr_sv_above_eps",
-            "ID_rank",
-        ]
-        float_attributes = ["beta", "delta_t", "eps", "h", "phi", "singular_values"]
-        array_attributes = [
-            "times",
-            "fine_grid",
-            "kernel",
-            "idx",
-            "proj",
-            "coarse_grid",
-        ]
-
-        for member in integer_attributes:
-            setattr(self, member, 0)
-        for member in float_attributes:
-            setattr(self, member, 0.0)
-        for member in array_attributes:
-            setattr(self, member, np.array([]))
-
-        # ____________________________________________________
 
     def get_coarse_grid(self):
         """
