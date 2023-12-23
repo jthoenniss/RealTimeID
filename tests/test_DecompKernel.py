4a61085086fc34f602f9d8f28a875b9c6fd3e9e2
Julian Thoenniss <julthoe@gmail.com>
2023-12-23 10:05:27 +0100

    add test that checks that all necessary attributes are present in DecompKernel object

--- tests/test_DecompKernel.py
+++ tests/test_DecompKernel.py
@@ -6,6 +6,7 @@ from src.discr_error.discr_error import DiscrError
 
 
 class TestDecompKernel_with_kwargs(unittest.TestCase):
+    
     def setUp(self):
         # Common setup that can be used across multiple tests
 
@@ -13,8 +14,8 @@ class TestDecompKernel_with_kwargs(unittest.TestCase):
             m=10,
             n=5,
             beta=1.0,
-            N_max = 10,
-            delta_t = 0.1,
+            N_max=10,
+            delta_t=0.1,
             eps=0.1,
             h=0.2,
             phi=np.pi / 4,
@@ -27,7 +28,9 @@ class TestDecompKernel_with_kwargs(unittest.TestCase):
         self.assertEqual(self.K.beta, 1.0)
         self.assertEqual(self.K.N_max, 10)
         self.assertEqual(self.K.delta_t, 0.1)
-        self.assertTrue(np.array_equal(self.K.times, cf.set_time_grid(N_max=10, delta_t=0.1)))
+        self.assertTrue(
+            np.array_equal(self.K.times, cf.set_time_grid(N_max=10, delta_t=0.1))
+        )
         self.assertEqual(self.K.eps, 0.1)
         self.assertEqual(self.K.h, 0.2)
         self.assertEqual(self.K.phi, np.pi / 4)
@@ -48,31 +51,58 @@ class TestDecompKernel_with_kwargs(unittest.TestCase):
         self.assertEqual(K.shape, expected_shape)
 
     def test_perform_SVD_maxrank(self):
-        # Test SVD for corner case of error eps = 0.0 
-        num_sv, sv = self.K.perform_SVD(eps = 0.0)
+        # Test SVD for corner case of error eps = 0.0
+        num_sv, sv = self.K.perform_SVD(eps=0.0)
         expected_num_sv = min(self.K.kernel.shape[0], self.K.kernel.shape[1])
         self.assertEqual(num_sv, expected_num_sv)
 
     def test_perform_SVD_minrank(self):
         # Test SVD for corner case of error eps = 1.0
-        num_sv, sv = self.K.perform_SVD(eps = 1.0)
+        num_sv, sv = self.K.perform_SVD(eps=1.0)
         expected_num_sv = 0
         self.assertEqual(num_sv, expected_num_sv)
-        
+
     def test_perform_ID_maxrank(self):
-        # Test SID for corner case of error eps = 0.0 
-        ID_rank, idx, proj = self.K.perform_ID(eps = 0.0)
+        # Test SID for corner case of error eps = 0.0
+        ID_rank, idx, proj = self.K.perform_ID(eps=0.0)
         expected_ID_rank = min(self.K.kernel.shape[0], self.K.kernel.shape[1])
         self.assertEqual(ID_rank, expected_ID_rank)
 
+    def test_get_vars(self):
+        #set of all keys required to be present in DecompKernel object
+        REQUIRED_KEYS = {
+            "m",
+            "n",
+            "beta",
+            "N_max",
+            "delta_t",
+            "h",
+            "phi",
+            "ID_rank",
+            "proj",
+            "idx",
+            "fine_grid",
+            "coarse_grid",
+            "singular_values",
+            "nbr_sv_above_eps",
+        }
+
+        # dictionary of all class attributes
+        attrs = vars(self.K)
+
+        # check that all required keys are present in kernel object
+        for key in REQUIRED_KEYS:
+            self.assertTrue(
+                key in attrs.keys(), f"Key '{key}' not found in DecompKernel"
+            )
 
 
 class TestDecompKernel_with_DiscError(unittest.TestCase):
     """
-    This test class checks if initialization is equivalent when using kwargs and when using an instance of DiscrError 
+    This test class checks if initialization is equivalent when using kwargs and when using an instance of DiscrError
     """
-    def setUp(self):
 
+    def setUp(self):
         params_DiscrError = {
             "m": 10,
             "n": 5,
@@ -81,31 +111,47 @@ class TestDecompKernel_with_DiscError(unittest.TestCase):
             "delta_t": 0.1,
             "h": 0.2,
             "phi": np.pi / 4,
-            "upper_cutoff" : 600
+            "upper_cutoff": 600,
         }
         D = DiscrError(**params_DiscrError)
 
         self.dck = DecompKernel(D)
 
-        #Add eps to params, such that both initilizations should be equivalent,
-        #Also: pop upper_cutoff
+        # Add eps to params, such that both initilizations should be equivalent,
+        # Also: pop upper_cutoff
         params_DiscrError["eps"] = D.eps
         params_DiscrError.pop("upper_cutoff")
 
         self.dck_kwargs = DecompKernel(**params_DiscrError)
 
     def test_base_attrs_present(self):
-        KernelMatrix_keys_required = ["m","n","beta","N_max","delta_t", "h","phi","times","fine_grid","k_values","kernel"]
+        KernelMatrix_keys_required = [
+            "m",
+            "n",
+            "beta",
+            "N_max",
+            "delta_t",
+            "h",
+            "phi",
+            "times",
+            "fine_grid",
+            "k_values",
+            "kernel",
+        ]
 
         present_keys = vars(self.dck).keys()
-        for key in KernelMatrix_keys_required:#check that all attributes exist.
+        for key in KernelMatrix_keys_required:  # check that all attributes exist.
             self.assertTrue(key in present_keys, f"Key {key} not found.")
-
 
     def test_initialization(self):
         # check that initialization is equivalent to initialization with kwargs
         for key, val in vars(self.dck).items():
-            self.assertEqual(np.all(val), np.all(getattr(self.dck_kwargs, key)), f"The following attributes differs: {key},{val}, {getattr(self.dck_kwargs, key)}")
+            self.assertEqual(
+                np.all(val),
+                np.all(getattr(self.dck_kwargs, key)),
+                f"The following attributes differs: {key},{val}, {getattr(self.dck_kwargs, key)}",
+            )
+
 
 if __name__ == "__main__":
     unittest.main()
