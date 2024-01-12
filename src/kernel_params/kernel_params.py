d6782864e57aff5ccc5b82fdb7ecfa04064ebd50
Julian Thoenniss <julthoe@gmail.com>
2024-01-12 12:15:34 +0100

    add 'upper_cutoff_discrete' and 'lower_cutoff_discrete' to parameter list

--- src/kernel_params/kernel_params.py
+++ src/kernel_params/kernel_params.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math  # for floor and ceiling
+from typing import Any #for type hinting
 
 class KernelParams:
 
@@ -18,9 +19,12 @@ class KernelParams:
         - delta_t (float): Time step for the time grid.
         - beta (float): Inverse temperature, a crucial thermodynamic parameter.
         - upper_cutoff (float): Frequency cutoff for continuous integrations.
+        - upper_cutoff_discrete (float): Frequency cutoff for discrete integrations.
+        - lower_cutoff_discrete (float): Frequency cutoff for discrete integrations.
         - h (float): Discretization parameter, influencing frequency grid resolution.
         - phi (float): Rotation angle in the complex frequency plane.
         - spec_dens (callable): Single-parameter function returning the spectral density.
+        
 
     Attributes:
         _params (dict): A dictionary that holds the parameter values.
@@ -32,23 +36,24 @@ class KernelParams:
     """
     
     def __init__(self, **kwargs):
+
+        #Initialize dictionary of parameters to default values
         self._params = {
             "m": None,
             "n": None,
             "N_max": None,
             "delta_t": 0.1,
             "beta": np.inf,
-            "upper_cutoff": np.inf,
+            "upper_cutoff": np.inf, 
+            "upper_cutoff_discrete": 15.0, # with this choice, the frequency grid reaches up to 1.e6 or higher.
+            "lower_cutoff_discrete": 3.6, # with this choice, the frequency grid reaches down to 1.e-16 or lower.
             "h": None,
             "phi": np.pi / 4,
             "spec_dens": lambda x: 1.
         }    
 
-        for key, value in kwargs.items():
-            if key in self._params.keys():
-                self._params[key] = value
-            else:
-                raise KeyError(f"Key {key} not in parameter list.")
+        # For those parameters that are specified as keyword arguments, update the values
+        self._params.update((k, v) for k, v in kwargs.items() if k in self._params)
 
     @property
     def params(self) -> dict:
@@ -57,13 +62,17 @@ class KernelParams:
         """
         return self._params
     
-    def get(self, key: str):
+    def get(self, key: str) -> Any:
         """
         Get the value of a parameter by providing its key
         """
+
+        if key not in self._params.keys():
+            raise KeyError(f"Key {key} not in parameter list.")
+
         return self._params.get(key)
 
-    def update_parameters(self, updates):
+    def update_parameters(self, updates: dict) -> None:
         """
         Update grid parameters with new values.
 
@@ -72,20 +81,29 @@ class KernelParams:
 
         Returns:
         - None
+
+        Note:
+        - A warning is issued if 'h', 'm', and 'n' parameters are attempted to be updated simultaneously, 
+          as this may lead to unintended consequences in the grid configuration.
         """
 
-        for name, value in updates.items():
+        if not isinstance(updates, dict):
+            raise TypeError(f"Input must be of type dict, not {type(updates)}.")
+        
+        for key, value in updates.items():
+            
+            if key not in self._params.keys():
+                raise KeyError(f"Key {key} not in parameter list.")
+            
+            self._params[key] = value #update parameter value
 
-            self._params[name] = value
-            if name == "h":
-                # when h is varied, also update m and n
-                self._params["m"] = math.ceil(15.0 / value) # with this choice, the frequency grid reaches up to 1.e8 or higher.
-                self._params["n"] = math.ceil(3.6 / value) # with this choice, the frequency grid reaches down to 1.e-16 or lower.
+            if key == "h":  # Special handling when 'h' is updated
+                # Check for simultaneous updates to 'm' and 'n'
+                if "m" in updates or "n" in updates:
+                    intersection = set(updates).intersection({"h", "m", "n"})
+                    print(f"Warning: Simultaneous updates to parameters {intersection} detected. Please double-check for consistency.")
 
-        # If m and n are varied simultaneously, throw warning.
-        updated_params = set(updates.keys())
-        intersection = updated_params.intersection({"h", "m", "n"})
-        if len(intersection) > 1:
-            print(
-                f"Warning: Attempting to update the parameters {intersection} simultaneously. Double check that this is intended."
-            )
+                # Update 'm' and 'n' based on the new value of 'h'
+                self._params["m"] = math.ceil(self.get("upper_cutoff_discrete") / value) 
+                self._params["n"] = math.ceil(self.get("lower_cutoff_discrete") / value) 
+
