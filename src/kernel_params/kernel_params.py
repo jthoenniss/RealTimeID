40f1b1e6dd77cffe148eac2c43c6fcfa6a4a0a6c
Julian Thoenniss <julthoe@gmail.com>
2024-01-12 12:45:47 +0100

    incorporated parameter validator in kernel_params class

--- src/kernel_params/kernel_params.py
+++ src/kernel_params/kernel_params.py
@@ -3,7 +3,7 @@ import math  # for floor and ceiling
 from typing import Any #for type hinting
 
 class KernelParams:
-
+    MAX_EPS = 1.0
     """
     A class designed to encapsulate and manage parameters essential for specifying a kernel matrix
     and its associated continuous integration results.
@@ -11,6 +11,8 @@ class KernelParams:
     This class serves as a container for key parameters used in kernel matrix calculations, such as
     discretization intervals, inverse temperature, time grid settings, discretization parameter, and
     rotation angle in the complex plane.
+
+    It provides a number of static functions to validate parameter values
 
     Parameters:
         - m (int): Number of discretization intervals for frequencies greater than 1/e.
@@ -33,6 +35,8 @@ class KernelParams:
         params() -> dict: Returns the dictionary of stored parameters.
         get(key: str) -> Any: Retrieves the value of a specific parameter using its key.
         update_parameters(updates: dict) -> None: Updates multiple parameters simultaneously based on a dictionary of updates.
+
+        + a list of static methods for parameter validation
     """
     
     def __init__(self, **kwargs):
@@ -126,3 +130,82 @@ class KernelParams:
                 self._params["m"] = math.ceil(self.get("upper_cutoff_discrete") / value) #set value of m such that is reaches up to upper_cutoff_discrete for given h
                 self._params["n"] = math.ceil(self.get("lower_cutoff_discrete") / value) #set value of n such that is reaches down to lower_cutoff_discrete for given h
 
+
+    @staticmethod
+    def validate_m_n(m: int, n: int):
+        if not isinstance(m, int) or m < 0:
+            raise ValueError(f"'m' must be a non-negative integer, got {m}")
+        if not isinstance(n, int) or n < 0:
+            raise ValueError(f"'n' must be a non-negative integer, got {n}")
+
+    @staticmethod
+    def validate_beta(beta: float):
+        if not isinstance(beta, (float, int)) or beta < 0:
+            raise ValueError(f"'beta' must be a positive number, got {beta}")
+
+    @staticmethod
+    def validate_times(times: np.ndarray):
+        if not isinstance(times, np.ndarray) or times.ndim != 1 or times.size == 0:
+            raise ValueError("Attribute 'times' must be a non-empty 1D NumPy array.")
+   
+
+    @staticmethod
+    def validate_N_max_and_delta_t(N_max: int, delta_t: float):
+        if not isinstance(N_max, (int, np.int64)):
+            raise ValueError(f"Attribute 'N_max' must be an int, got {type(N_max).__name__}.")
+
+        if not isinstance(delta_t, (float, int, np.float64)):
+            raise ValueError(f"Attribute 'delta_t' must be a float or int, got {type(delta_t).__name__}.")
+
+        if N_max <= 0:
+            raise ValueError(f"Attribute 'N_max' must be positive, got {N_max}.")
+
+        if delta_t <= 0:
+            raise ValueError(f"Attribute 'delta_t' must be positive, got {delta_t}.")
+            
+    @staticmethod
+    def validate_eps(eps: float):
+        if not 0 < eps < KernelParams.MAX_EPS:
+            raise ValueError(f"'eps' must be between 0 and {KernelParams.MAX_EPS}, got {eps}")
+
+    @staticmethod
+    def validate_h(h: float):
+        if not isinstance(h, (float, int)) or h <= 0:
+            raise ValueError(f"'h' must be a positive number, got {h}")
+
+    @staticmethod  
+    def validate_phi(phi: float):
+        if not isinstance(phi, (float, int)):
+            raise ValueError(f"'phi' must be a number, got {phi}")
+
+    @staticmethod  
+    def validate_upper_cutoff(upper_cutoff: float):
+        if not isinstance(upper_cutoff, (float, int)) or upper_cutoff <= 0:
+            raise ValueError(f"'upper_cutoff' must be a positive number, got {upper_cutoff}")
+        
+    @staticmethod  
+    def validate_upper_cutoff_discrete(upper_cutoff_discrete: float):
+        if not isinstance(upper_cutoff_discrete, (float, int)) or upper_cutoff_discrete <= 0:
+            raise ValueError(f"'upper_cutoff' must be a positive number, got {upper_cutoff_discrete}")
+        
+    @staticmethod  
+    def validate_lower_cutoff_discrete(lower_cutoff_discrete: float):
+        if not isinstance(lower_cutoff_discrete, (float, int)) or lower_cutoff_discrete <= 0:
+            raise ValueError(f"'lower_cutoff' must be a positive number, got {lower_cutoff_discrete}")
+        
+
+    @staticmethod
+    def validate_required_params(params, required_params):
+        """
+        Validate if all parameters specifided in 'required_parameters' are present.
+
+        Parameters:
+            params (dict): Parameters to validate.
+            required_params (list): List of required parameter names.
+        
+        Raises:
+            ValueError: If any required parameter is missing.
+        """
+        missing_params = [p for p in required_params if p not in params]
+        if missing_params:
+            raise ValueError(f"Missing required parameter(s): {', '.join(missing_params)}")
