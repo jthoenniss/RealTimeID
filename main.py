52e88f3f877f1b5361822f36fcc5533917681819
Julian Thoenniss <julthoe@gmail.com>
2024-01-16 17:20:41 +0100

    add time stemps and message for start and stop.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy as np
 from src.utils.module_utils.all_custom_modules import Hdf5Kernel
 from src.kernel_params.kernel_params import KernelParams
 from src.generate_data.data_generation_functions import compute_grid_and_store
+import time
+
 
 """
 A script to compute the interpolative decomposition with all corresponding quantities on a parameter grid
@@ -17,16 +19,18 @@ To evaluate the hdf5 file, see jupyter notebook "/notebooks/analyze_ID_kernel.ip
 
 if __name__ == "__main__":
 
+    time_init = time.time()
+
     # _______________Set Parameter Grid (choose values to explore)____________________
     # Array specifying all values for the discreitzation parameter, h, that should be evaluated
     h_vals = np.logspace(-2, -.2, 15)
     # Array specifying all values for the total number of time steps, N_max, that should be evaluated
-    N_maxs = list(map(int, np.logspace(1, 5, 10)))
+    N_maxs = list(map(int, np.logspace(1, 2, 10)))
     # Array specifying all values for inverse temperature, beta, that should be evaluated
     betas = [0, 20, 100, 10000]
 
     # Define filename of hdf5 file holding the data
-    filename = f"data/delta_t=0.1_large.h5"
+    filename = f"data/delta_t=0.1_rel_error=0.01_large.h5"
 
     # Create instance of Hdf5Kernel to be associated with the file
     h5_kernel = Hdf5Kernel(filename=filename)
@@ -37,8 +41,18 @@ if __name__ == "__main__":
 
     # Create instance of KernelParams to hold the parameter set (initialize with default values unless keyword arguments are specified)
     params = KernelParams()
+
+    print(f"Starting computation of data on parameter grid with dimensions [h: {param_grid_dims[0]}, N_max: {param_grid_dims[1]}, beta: {param_grid_dims[2]}].")
     # compute data and write to file
     compute_grid_and_store(
-        h_vals=h_vals, N_maxs=N_maxs, betas=betas, params=params, h5_kernel=h5_kernel, optimize=True, rel_error_diff=1.e-16
+        h_vals=h_vals, N_maxs=N_maxs, betas=betas, params=params, h5_kernel=h5_kernel, optimize=True, rel_error_diff=0.01
     )
+
+    run_time = time.time() - time_init
+    #convert runtime from seconds to hours, minutes, seconds
+    hours = round(run_time // 3600)
+    minutes = round((run_time % 3600) // 60)
+    seconds = round(run_time % 60)
+
+    print(f"Finished computation of data on parameter grid.\n Total runtime: {hours}h {minutes}m {seconds}s.")
 
