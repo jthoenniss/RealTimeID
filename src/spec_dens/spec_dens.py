823b536ae8fe09ac016e3c070827f30d203ad432
Julian Thoenniss <julthoe@gmail.com>
2024-01-17 15:31:13 +0100

    add gapped spectral density and corresponding unit tests

--- src/spec_dens/spec_dens.py
+++ src/spec_dens/spec_dens.py
@@ -2,31 +2,80 @@ import numpy as np
 
 MAX_EXP_ARG = np.log(np.finfo(np.float64).max)
 
+
 # define a collection of vectorized spectral densities
-def spec_dens_gapless(omega, cutoff_lower: float = -100., cutoff_upper: float = 100.0, Gamma: float = 1.0) -> float: 
-        """
-        Gapless spectral density with smooth cutoffs.
-        Sharpness of cutoff is controlled by 'sharpness'.
+def spec_dens_gapless(
+    omega: np.ndarray,
+    cutoff_lower: float = -10.0,
+    cutoff_upper: float = 10.0,
+    Gamma: float = 1.0,
+) -> np.ndarray:
+    """
+    Gapless spectral density with smooth cutoffs.
+    Sharpness of cutoff is controlled by 'sharpness'.
 
-        Vectorized implementation.
-        
+    Vectorized implementation.
 
-        Parameters:
-        - cutoff_lower (float, optional): Lower cutoff frequency. Should be negative.
-        - cutoff_upper (float, optional): Upper cutoff frequency. Should be positive.
-        - Gamma (float, optional): Energy scale of the spectral density.
-   
-        Returns:
-        scalar/np.ndarray: Spectral density evaluated at the specified frequency values.
-        """ 
 
-        sharpness = 10 * Gamma
-        
-        safe_omega_upper = np.clip(sharpness * (omega - cutoff_upper), None,  MAX_EXP_ARG)
-        safe_omega_lower = np.clip(- sharpness * (omega - cutoff_lower), None, MAX_EXP_ARG)
+    Parameters:
+    - omega (scalar/np.ndarray): Frequency values at which the spectral density is evaluated.
+    - cutoff_lower (float, optional): Lower cutoff frequency.
+    - cutoff_upper (float, optional): Upper cutoff frequency.
+    - Gamma (float, optional): Energy scale of the spectral density.
 
-        denominator = (1 + np.exp(safe_omega_lower)) * (1 + np.exp(safe_omega_upper))
-       
-        val = Gamma / denominator
-        
-        return val
+    Returns:
+    scalar/np.ndarray: Spectral density evaluated at the specified frequency values.
+    """
+    #hard code sharpness
+    sharpness = 50 * Gamma
+
+    safe_omega_upper = np.clip(sharpness * (omega - cutoff_upper), None, MAX_EXP_ARG)
+    safe_omega_lower = np.clip(-sharpness * (omega - cutoff_lower), None, MAX_EXP_ARG)
+
+    denominator = (1 + np.exp(safe_omega_lower)) * (1 + np.exp(safe_omega_upper))
+
+    val = Gamma / denominator
+
+    return val
+
+
+def spec_dens_gapped_sym(
+    omega: np.ndarray,
+    cutoff_lower: float = 2.0,
+    cutoff_upper: float = 10.0,
+    Gamma: float = 1.0,
+) -> np.ndarray:
+    """
+    Gapped spectral density with smooth cutoffs. Symmetric around 0.
+    Parameters specify the spectral density for positive frequencies.
+
+    Parameters:
+    - cutoff_lower (float, optional): Lower cutoff frequency for positive frequencies. Should be positive.
+    - cutoff_upper (float, optional): Upper cutoff frequency for positive frequencies. Should be positive.
+    - Gamma (float, optional): Energy scale of the spectral density.
+
+    Returns:
+    scalar/np.ndarray: Spectral density evaluated at the specified frequency values.
+    """
+
+    val = spec_dens_gapless(
+        omega, cutoff_lower=cutoff_lower, cutoff_upper=cutoff_upper, Gamma=Gamma
+    ) + spec_dens_gapless(
+        omega, cutoff_lower=-cutoff_upper, cutoff_upper=-cutoff_lower, Gamma=Gamma
+    )
+
+    return val
+
+
+if __name__ == "__main__":
+ 
+    # plot spectral densities
+    import matplotlib.pyplot as plt
+
+    x = np.linspace(-15, 15, 1000)
+    plt.plot(x, spec_dens_gapless(x), linewidth = 3, label="gapless")
+    plt.plot(x, spec_dens_gapped_sym(x), linewidth = 1.5, linestyle = 'dashed', label="gapped")
+    plt.legend(loc = "upper right")
+    plt.xlabel("Frequency " + r"$\omega$")
+    plt.ylabel("Spectral density " + r"$\Gamma(\omega)$")
+    plt.show()
--- tests/test_spec_dens.py
+++ tests/test_spec_dens.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from src.spec_dens.spec_dens import spec_dens_gapless
+from src.spec_dens.spec_dens import spec_dens_gapless, spec_dens_gapped_sym
 
 
 class Test_spec_dens_gapless(unittest.TestCase):
@@ -16,15 +16,18 @@ class Test_spec_dens_gapless(unittest.TestCase):
         """
         Test the vectorized implementation of the spectral density of the gapped model.
         """
+        #______Define frequency grids for different frequency regions______
+        #above the bulk
+        omegas_above_cutoff = np.linspace( self.cutoff_upper + 1, self.cutoff_upper + 100 , 10)
+        #below the bulk
+        omegas_below_cutoff = np.linspace(self.cutoff_lower - 100 , self.cutoff_lower - 1, 10)
+        #in the bulk
+        omegas_between_cutoffs = np.linspace(self.cutoff_lower + 1, self.cutoff_upper - 1, 10)
         
-        
-        omegas_above_cutoff = np.linspace(1.2 * self.cutoff_upper, 5 * self.cutoff_upper , 10)
-        omegas_below_cutoff = np.linspace(5 * self.cutoff_lower , 1.2 * self.cutoff_lower, 10)
-        omegas_between_cutoffs = np.linspace(0.8 * self.cutoff_lower, 0.8 * self.cutoff_upper, 10)
-        
-
-        # check if spectral density is zero beyond cutoffs
+        #_________Test implementation of spectral density_________
+        #above the bulk
         self.assertTrue(np.all(np.isclose(self.spec_dens(omegas_above_cutoff),0. , rtol=1e-10)))
+        #below the bulk
         self.assertTrue(np.all(np.isclose(self.spec_dens(omegas_below_cutoff),0. , rtol=1e-10)))
         # check if spectral density is Gamma between cutoffs
         self.assertTrue(np.all(np.isclose(self.spec_dens(omegas_between_cutoffs), self.Gamma , rtol=1e-10)))
@@ -35,10 +38,52 @@ class Test_spec_dens_gapless(unittest.TestCase):
         """
         Test the implementation of the spectral density of the gapped model for scalar frequency arguments
         """
-        
-        self.assertTrue(np.isclose(self.spec_dens(0), self.Gamma, rtol=1e-10))
-        self.assertTrue(np.isclose(self.spec_dens(1.2 * self.cutoff_lower), 0., rtol=1e-10))
-        self.assertTrue(np.isclose(self.spec_dens(1.2 * self.cutoff_upper), 0., rtol=1e-10))
+        #in the center of the bulk
+        self.assertTrue(np.isclose(self.spec_dens(self.cutoff_lower + (self.cutoff_upper - self.cutoff_lower) / 2 ), self.Gamma, rtol=1e-10))
+        #below the bulk
+        self.assertTrue(np.isclose(self.spec_dens(self.cutoff_lower - 1), 0., rtol=1e-10))
+        #above the bulk
+        self.assertTrue(np.isclose(self.spec_dens(self.cutoff_upper + 1), 0., rtol=1e-10))
     
+
+
+class Test_spec_dens_gapped(unittest.TestCase):
+
+    def setUp(self):
+        # define test parameters
+        self.cutoff_lower = 2.#lower cutoff frequency for positive frequencies (defines upper cutoff of the gap)
+        self.cutoff_upper = 10. #upper cutoff frequency for positive frequencies
+        self.Gamma = 1.0
+        self.spec_dens = lambda x: spec_dens_gapped_sym(x, cutoff_lower= self.cutoff_lower, cutoff_upper = self.cutoff_upper, Gamma = self.Gamma)
+    
+    def test_spec_dens_gapped_vectorized(self):
+        """
+        Test the vectorized implementation of the spectral density of the gapped model.
+        """
+        
+        #______Define frequency grids for different frequency regions______
+        #above the upper bulk
+        omegas_above_upper_bulk = np.linspace( self.cutoff_upper + 1, self.cutoff_upper + 100 , 10)
+        #in the upper bulk
+        omegas_in_upper_bulk = np.linspace( self.cutoff_lower + 1 , self.cutoff_upper - 1, 10)
+        #in the gap (below upper bulk and above lower bulk)
+        omegas_in_gap = np.linspace(- self.cutoff_lower + 1, self.cutoff_lower - 1, 10)
+        #in the lower bulk
+        omegas_in_lower_bulk = np.linspace( -self.cutoff_upper + 1 , -self.cutoff_lower - 1, 10)
+        #below the lower bulk
+        omegas_below_lower_bulk = np.linspace( -self.cutoff_upper -100 , -self.cutoff_upper - 1 , 10)
+        
+        #_________Test implementation of spectral density_________  
+        #check if spectral density is zero above upper bulk
+        self.assertTrue(np.all(np.isclose(self.spec_dens(omegas_above_upper_bulk), 0. , rtol=1e-10)))
+        #check if spectral density is Gamma below upper bulk
+        self.assertTrue(np.all(np.isclose(self.spec_dens(omegas_in_upper_bulk), self.Gamma , rtol=1e-10)))
+        #check if spectral density is zero in gap
+        self.assertTrue(np.all(np.isclose(self.spec_dens(omegas_in_gap), 0. , rtol=1e-10)))
+        #check is spectral density is Gamma in lower bulk
+        self.assertTrue(np.all(np.isclose(self.spec_dens(omegas_in_lower_bulk), self.Gamma , rtol=1e-10)))
+        #check if spectral density is zero below lower bulk
+        self.assertTrue(np.all(np.isclose(self.spec_dens(omegas_below_lower_bulk), 0. , rtol=1e-10)))
+
 if __name__ == "__main__":
     unittest.main()
