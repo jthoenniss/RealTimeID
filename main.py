9a14c8b984cb11874f00fb53a0218eeffe5ca1dd
Julian Thoenniss <julthoe@gmail.com>
2024-01-18 18:17:08 +0100

    update default parameters for beta grid

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 from src.utils.module_utils.all_custom_modules import Hdf5Kernel
 from src.kernel_params.kernel_params import KernelParams
 from src.generate_data.data_generation_functions import compute_grid_and_store
+from src.spec_dens.spec_dens import spec_dens_gapless, spec_dens_gapped_sym
 import time
 
 
@@ -27,10 +28,10 @@ if __name__ == "__main__":
     # Array specifying all values for the total number of time steps, N_max, that should be evaluated
     N_maxs = list(map(int, np.logspace(1, 2, 10)))
     # Array specifying all values for inverse temperature, beta, that should be evaluated
-    betas = [0, 20, 100, 10000]
+    betas = [0, 1.e0, 1.e1, 1.e2, 1.e3, 1.e4, 1.e5]
 
     # Define filename of hdf5 file holding the data
-    filename = f"data/delta_t=0.1_rel_error=0.01_large.h5"
+    filename = f"data/delta_t=0.1_gapless.h5"
 
     # Create instance of Hdf5Kernel to be associated with the file
     h5_kernel = Hdf5Kernel(filename=filename)
@@ -42,7 +43,7 @@ if __name__ == "__main__":
     # Create instance of KernelParams to hold the parameter set (initialize with default values unless keyword arguments are specified)
     params = KernelParams()
 
-    print(f"Starting computation of data on parameter grid with dimensions [h: {param_grid_dims[0]}, N_max: {param_grid_dims[1]}, beta: {param_grid_dims[2]}].")
+    print(f"Starting computation of data on parameter grid with dimensions {param_grid_dims}.")
     # compute data and write to file
     compute_grid_and_store(
         h_vals=h_vals, N_maxs=N_maxs, betas=betas, params=params, h5_kernel=h5_kernel, optimize=True, rel_error_diff=0.01
