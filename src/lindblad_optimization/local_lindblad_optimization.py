edcea1dd175eda7703586b1b960ece7ad6a6b8f9
Julian Thoenniss <julthoe@gmail.com>
2024-03-14 20:06:02 +0100

    get lindblad optimization running

--- src/lindblad_optimization/local_lindblad_optimization.py
+++ src/lindblad_optimization/local_lindblad_optimization.py
@@ -1,170 +1,143 @@
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 
 
 # define the propagator function that takes a list of parameters and a time grid and returns the propagator
-def propag_from_params(parameters: tf.Tensor, time_grid: tf.Tensor) -> tf.Tensor:
-    assert time_grid.shape[1] == 1, "Time grid must be a column vector (i.e., have shape (n, 1))."
+def propag_from_params(time_grid: tf.Tensor, Gamma_k: tf.Tensor, omega_k: tf.Tensor, gamma_k: tf.Tensor) -> tf.Tensor:
+    assert time_grid.shape[1] == 1, f"Time grid must be a column vector (i.e., have shape (n, 1)), but detected shape is {time_grid.shape}."
 
-    Gamma_k = parameters[0]  # Gamma_k is used as real in complex multiplication later.
-    omega_k = parameters[1]
-    gamma_k = parameters[2]
+    real_part = tf.reduce_sum(tf.exp(-gamma_k * time_grid) * (Gamma_k * tf.cos(omega_k * time_grid)), axis=1)
+    imag_part = tf.reduce_sum(tf.exp(-gamma_k * time_grid) * (Gamma_k * tf.sin(omega_k * time_grid)), axis=1)
 
-    # First, ensure omega_k and gamma_k are cast to complex
-    omega_k_complex = tf.cast(omega_k, tf.complex128)
-    gamma_k_complex = tf.cast(gamma_k, tf.complex128)
-    time_grid_complex = tf.cast(time_grid, tf.complex128)
+    #flatten both arrays and stack them to get a 2D tensor
+    real_part = tf.reshape(real_part, (1, -1))
+    imag_part = tf.reshape(imag_part, (1, -1))
 
-    # Now, perform the complex arithmetic with all operands as complex tensors
-    complex_exponent = 1j * omega_k_complex - gamma_k_complex
-    propag_mat = tf.cast(Gamma_k, tf.complex128) * tf.exp(complex_exponent * time_grid_complex)
-    propag_vec = tf.reduce_sum(propag_mat, axis=1)
+    #stack real and imaginary part in (2, len(time_grid)) tensor
+    propag_vec_real_valued = tf.stack([real_part, imag_part], axis=0)
 
-    return tf.reshape(propag_vec, (-1,))# return flattened vector
+    return propag_vec_real_valued
 
 
-
-
-# Define the propagator function that takes a list of parameters and a time grid and returns the propagator
-class PropagLayer(tf.keras.layers.Layer):
-    """ 
-    Custom layer that computes the propagator from a list of parameters and a time grid.
-    This is convenient because it allows us to use the keras API to optimize the parameters:
-    The parameters are the only trainable variables in the model.
-    """
-
-    def __init__(self, time_grid, **kwargs):
-        """
-        Initialize the layer with the time grid.
-        Args:
-        - time_grid (array): The time grid on which the propagator is calculated. Will be cast to a tf.Tensor of shape (n, 1).
-        - **kwargs: Additional keyword arguments for the layer.
-
-        Returns:
-        - None
-        """
-        super(PropagLayer, self).__init__(**kwargs)
-        self.time_grid = tf.cast(tf.expand_dims(time_grid, -1), tf.float64)  # converted to tensor flow column vector. Analogue to[:, np.newaxis] in numpy
-
-    def call(self, parameters):
-        # Convert python function using numpy to tensorflow function in order to use automatic differentiation
-        # (Automatic differentiation is only possible for tensorflow functions, not numpy functions.)
-        return propag_from_params(parameters, self.time_grid)
-
-# Define the model
 class PropagModel(tf.keras.Model):
-    """
-    Custom model that includes the PropagLayer. The parameters are the only trainable variables in the model.
-    """
-    def __init__(self, time_grid, initial_params):
-        """
-        Initialize the model with the time grid and the initial parameters.
-        Args:
-        - time_grid (array): The time grid on which the propagator is calculated.
-        - initial_params (array): The initial parameters for the model. 
-            First row: Gamma_k (coupling), 
-            second row: omega_k (unitary evolution), 
-            third row: gamma_k (decay).
-        Returns:
-        - None
-        """
-        #initialize the model with the time grid and the initial parameters
+    def __init__(self, time_grid: list, initial_Gamma_k: list, initial_omega_k: list, initial_gamma_k: list):
         super(PropagModel, self).__init__()
-        # Initialize the propagator layer
-        self.propag_layer = PropagLayer(time_grid)
-        # Initialize parameters as a trainable variable
-        self.params = tf.Variable(initial_params, dtype=tf.float64, trainable=True) 
 
+        self.time_grid = tf.reshape(tf.constant(time_grid, dtype=tf.float32), (-1, 1))# Ensure time grid is a column vector
+        
+        # Directly add constrained parameters to the model
+        self.Gamma_k = self.add_weight(name='Gamma_k',
+                                       shape=(len(initial_Gamma_k),),
+                                       initializer=tf.keras.initializers.Constant(initial_Gamma_k),
+                                       constraint=tf.keras.constraints.NonNeg(),
+                                       dtype=tf.float32,
+                                       trainable=True)
+        
+        self.omega_k = self.add_weight(name='omega_k',
+                                       shape=(len(initial_omega_k),),
+                                       initializer=tf.keras.initializers.Constant(initial_omega_k),
+                                       dtype=tf.float32,
+                                       trainable=True)
+        
+        self.gamma_k = self.add_weight(name='gamma_k',
+                                       shape=(len(initial_gamma_k),),
+                                       initializer=tf.keras.initializers.Constant(initial_gamma_k),
+                                       constraint=tf.keras.constraints.NonNeg(),
+                                       dtype=tf.float32,
+                                       trainable=True)
 
-    def call(self, x):
-        # Call the propagator layer with the parameters
-        return self.propag_layer(self.params)
+    def call(self, inputs):
+        return propag_from_params(self.time_grid, self.Gamma_k, self.omega_k, self.gamma_k)
+    
+    def target_vector(self, target_Gamma_k: tf.Tensor, target_omega_k: tf.Tensor, target_gamma_k: tf.Tensor) -> tf.Tensor:
+        return propag_from_params(self.time_grid, target_Gamma_k, target_omega_k, target_gamma_k)
     
     
-# Loss function: Sum of squared errors
-def custom_loss(y_true: tf.Tensor, y_pred: tf.Tensor) -> tf.Tensor:
+def custom_loss(y_true, y_pred):
     """
-    Function to compute the mean squared error between the target vector and the predicted vector.
+    Computes the mean squared error between y_true and y_pred, 
+  
     Parameters:
-    - y_true: tf.Tensor, the target vector
-    - y_pred: tf.Tensor, the predicted vector
-
+    - y_true: TensorFlow tensor containing the true values.
+    - y_pred: TensorFlow tensor containing the predicted values.
+   
     Returns:
-    - tf.Tensor: the loss between the two vectors (as real, positive number), [sum_i (|(y_true)_i - (y_pred)_i|)^2 ] / n, where n is the length of the vectors.
+    - A tensor containing the computed loss.
     """
-    return tf.losses.mean_squared_error(y_true, y_pred)
+    
+    # Compute the combined absolute values of the filtered values
+    difference = y_true - y_pred
 
+    diff_combined = tf.sqrt(tf.math.reduce_sum(tf.square(difference)))
+
+    #mse_loss = tf.math.reduce_mean(diff_combined)
+
+    return diff_combined
 
 class PrintParametersCallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
-        # Assuming 'params' is the attribute holding the parameters you're optimizing.
         # Adjust this to match how your model stores its parameters.
-        parameters = self.model.params
-        print(f"\n Parameters after epoch {epoch+1}: {parameters} \n")
-
+    
         logs = logs or {}
         error = logs.get('loss')
         print(f"Error at epoch {epoch}: {error}")
 
+class RecordParametersCallback(tf.keras.callbacks.Callback):
+    def __init__(self):
+        super(RecordParametersCallback, self).__init__()
+        # Initialize a dictionary to hold lists of parameter values
+        self.parameters_over_time = {}
+        
+    def on_epoch_end(self, epoch, logs=None):
+        # Record the parameters of interest
+        # For example, to record the value of 'Gamma_k'
+        if 'Gamma_k' not in self.parameters_over_time:
+            self.parameters_over_time['Gamma_k'] = []
+        if 'omega_k' not in self.parameters_over_time:
+            self.parameters_over_time['omega_k'] = []
+        if 'gamma_k' not in self.parameters_over_time:
+            self.parameters_over_time['gamma_k'] = []
+
+
+        self.parameters_over_time['Gamma_k'].append(self.model.Gamma_k.numpy())
+        self.parameters_over_time['omega_k'].append(self.model.omega_k.numpy())
+        self.parameters_over_time['gamma_k'].append(self.model.gamma_k.numpy())
+
+
+
 
 if __name__== "__main__":
 
+    nbr_epochs = 1500
 
+    #define time grid and parameters for target vector
+    # consider only two modes, whose parameters are given by the following:
+    # (columns correspond to different modes, rows to different parameters: Gamma, omega, and gamma)
     Gamma_1, Gamma_2 = 1., 2.
     omega_1, omega_2 = 3., 4.
     gamma_1, gamma_2 = 5., 6.
-    
 
-    parameters = np.array([[Gamma_1, Gamma_2], [omega_1, omega_2], [gamma_1, gamma_2]])
+    target_Gamma_k = [Gamma_1, Gamma_2]
+    target_omega_k = [omega_1, omega_2]
+    target_gamma_k = [gamma_1, gamma_2]
 
     # array of time points
-
-    time_grid_np = np.arange(0.1, 10., .05) * np.pi
-    time_grid = time_grid_np.tolist()
- 
-    # expected propagator. Compute separately for both modes
-    propag_mode1 = Gamma_1 * np.exp(
-        (1.0j * omega_1 - gamma_1)
-        * time_grid_np
-    )
-    propag_mode2 = Gamma_2 * np.exp(
-        (1.0j * omega_2 - gamma_2)
-        * time_grid_np
-    )
-
-    # create matrix where each column corresponds to the propagator of a mode:
-    propagmatrix = np.hstack(
-        [propag_mode1[:, np.newaxis], propag_mode2[:, np.newaxis]]
-    )  # shape (6, 2)
-
-    # sum over the columns to get the expected propagator:
-    expected_propag = np.sum(propagmatrix, axis=1)
-    
-    
-
-    # Define your target vector and time grid
-    target_vector = tf.constant(expected_propag, dtype=tf.complex128)
-   
-    #generate random  array of length 100:
-    Gamma_inits = np.abs(np.random.rand(1000).tolist())
-    omega_inits = np.random.rand(1000).tolist()
-    gamma_inits = np.abs(np.random.rand(1000).tolist())
-
-    
-    initial_params = [Gamma_inits, omega_inits, gamma_inits]
-
-    
-    # Create an instance of your custom callback
-    #print_parameters_callback = PrintParametersCallback()
+    time_grid = np.arange(.1,.5,.05).tolist() #make it a column vector
 
     # Instantiate model
-    model = PropagModel(time_grid, initial_params)
+    initial_Gamma_k = [1.2 * Gamma_1, Gamma_2]
+    initial_omega_k =  [omega_1, omega_2]
+    initial_gamma_k = [gamma_1, gamma_2]
+    
+    # Instantiate model
+    model = PropagModel(time_grid=time_grid, initial_Gamma_k=initial_Gamma_k, initial_omega_k=initial_omega_k, initial_gamma_k=initial_gamma_k)
     
      
     
     # Before your model compilation, specify a custom learning rate
-    custom_learning_rate = 0.001  # This is an example value; adjust based on your needs
+    custom_learning_rate = 0.01  # This is an example value; adjust based on your needs
 
     # Instantiate the Adam optimizer with the custom learning rate
     optimizer = tf.keras.optimizers.Adam(learning_rate=custom_learning_rate)
@@ -172,19 +145,53 @@ if __name__== "__main__":
     # Compile the model with the custom optimizer
     model.compile(optimizer=optimizer, loss=custom_loss)
 
-    x_dummy = tf.zeros((len(time_grid),), dtype=tf.complex64)
+    x_dummy = tf.zeros((2,len(time_grid)), dtype=tf.float32)
   
-    # Since the model technically doesn't "fit" to data in the traditional sense,
-    # we use a dummy input (x) and the target vector as y.
-    # The actual optimization occurs with respect to the parameters within the model.
-    # and specify callback
-    model.fit(x=x_dummy, y=target_vector, epochs=10000, callbacks=[PrintParametersCallback()])
+    model_init = model(x_dummy)
+    
+
+    target_vector  = model.target_vector(tf.constant(target_Gamma_k, dtype=tf.float32), tf.constant(target_omega_k, dtype=tf.float32), tf.constant(target_gamma_k, dtype=tf.float32))
+    
+    record_parameters_callback = RecordParametersCallback()
+    history = model.fit(x=x_dummy, y=target_vector, epochs=nbr_epochs, callbacks=[record_parameters_callback, PrintParametersCallback()])
 
 
-
+    print('error_init', custom_loss(target_vector, model_init))
    
-    print("output propag", model(tf.zeros((len(time_grid),), dtype=tf.complex64)))
-    print("target_vector", target_vector)   
-    got = model(tf.zeros((len(time_grid),), dtype=tf.complex64))
-    print("error",tf.losses.mean_squared_error(target_vector, got))
+    print("initial propag", model_init)
+    print("target propag", target_vector)   
+    got = model(x_dummy)
+    print("final propag", got)
+    print("final error," , custom_loss(target_vector, got))
+    print("trained parameters")
+    print(model.Gamma_k)
+    print(model.omega_k)
+    print(model.gamma_k)
+
+    #create a scatter plot where the dots are connected by a line
+    plt.plot(np.arange(0,nbr_epochs,10),history.history['loss'][::10], 'o-')
+
+    plt.title('Model Loss over Epochs')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train'], loc='upper right')
+    plt.ylim(bottom=-0.1)  
+    #add vertical line
+    plt.axhline(y=0, color='r', linestyle='--')
+    plt.show()
   
+
+
+    #plot the parameters over time
+    gamma_k_values = record_parameters_callback.parameters_over_time['Gamma_k']
+    plt.plot(gamma_k_values, label='Gamma_k')
+    omega_k_values = record_parameters_callback.parameters_over_time['omega_k']
+    plt.plot(omega_k_values, label='omega_k')
+    gamma_k_values = record_parameters_callback.parameters_over_time['gamma_k']
+    plt.plot(gamma_k_values, label='gamma_k')
+    plt.title('Parameters values over Epochs')
+    plt.legend()
+    plt.xlabel('Epoch')
+    #plt.axhline(y=np.pi, color='r', linestyle='--')
+    #plt.axhline(y=2*np.pi, color='r', linestyle='--')
+    plt.show()
