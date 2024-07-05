b2dbc917be9eaa5c1441fae0ed3468ad1c566d2a
Julian Thoenniss <julthoe@gmail.com>
2024-07-05 22:27:34 +0200

    add explicit poles to flat band spectral density. Update corresponding notebook

--- src/spec_dens/spec_dens.py
+++ src/spec_dens/spec_dens.py
@@ -82,6 +82,30 @@ class SpecDensGapless:
         - float: Pole frequency.
         """
         return omega + 2.0j * np.pi * 1 / self.sharpness * (k + 0.5)
+    
+    def _residue_pos(self, poles_pos):
+        """
+        Function to compute the residues of the spectral density multiplied with the Fermi-Dirac distribution.
+
+        Parameters:
+        - poles_pos (np.ndarray): Array of poles with positive real part.
+
+        Returns:
+        - np.ndarray: Residues of the spectral density multiplied with the Fermi-Dirac distribution.
+        """
+        return -1 / (1 + np.exp(-self.sharpness * (poles_pos + self.Lambda))) * 1 / self.sharpness
+    
+    def _residue_neg(self, poles_neg):
+        """
+        Function to compute the residues of the spectral density multiplied with the Fermi-Dirac distribution.
+
+        Parameters:
+        - poles_neg (np.ndarray): Array of poles with negative real part.
+
+        Returns:
+        - np.ndarray: Residues of the spectral density multiplied with the Fermi-Dirac distribution.
+        """
+        return 1 / (1 + np.exp(self.sharpness * (poles_neg - self.Lambda))) * 1 / self.sharpness
 
     def poles_residues(self, phi: float = np.pi / 4):
         """
@@ -112,21 +136,68 @@ class SpecDensGapless:
         )
 
         # residues of spectral density
-        residue_spec_dens_pos = (
-            -1
-            / (1 + np.exp(-self.sharpness * (poles_freq_table_pos + self.Lambda)))
-            * 1
-            / self.sharpness
-        )
-        residue_spec_dens_neg = (
-            1
-            / (1 + np.exp(self.sharpness * (poles_freq_table_neg - self.Lambda)))
-            * 1
-            / self.sharpness
-        )
+        residue_spec_dens_pos = self._residue_pos(poles_freq_table_pos)
+        residue_spec_dens_neg = self._residue_neg(poles_freq_table_neg)
+    
 
         poles = np.concatenate((poles_freq_table_neg, poles_freq_table_pos))
         residues = np.concatenate((residue_spec_dens_neg, residue_spec_dens_pos))
+
+        return poles, residues
+
+    def explicit_poles_and_residues(self, h: float, phi: float = np.pi/4):
+        """
+        Compute the "explicit" poles and residues that should always be considered in discrete sum along complex contour when approximating continous intgegral along complex path.
+
+        Parameters:
+        - phi (float, optional): Angle of the integration contour.
+
+        Returns:
+        - tuple: Poles and residues of the spectral density multiplied with the Fermi-Dirac distribution.
+        """
+
+        def _explicit_poles(phi_low, phi_up, Lambda, sharpness):
+            """
+            Returns:
+            - poles_pos: poles with positive real part
+            - poles_neg: poles with negative real part
+            """
+
+            #summation limits
+            limit_up = np.floor(1+np.tan(phi_up) * Lambda * sharpness / (2 * np.pi) - 1/2)
+            limit_low = np.floor(1+np.tan(phi_low) * Lambda * sharpness / (2 * np.pi) - 1/2)
+
+            #explicit poles between two rotation angles
+            #positive real part of frequencies
+            poles_pos = np.array([self._pole_freqs(omega = Lambda, k = k) for k in range(int(limit_low), int(limit_up))])
+            #negative real part of frequencies
+            poles_neg = np.array([self._pole_freqs(omega = -Lambda, k = k) for k in range(int(limit_low), int(limit_up))])
+
+            return poles_pos, poles_neg
+        
+    
+        add_poles_up_pos, add_poles_up_neg = _explicit_poles(phi_low = np.pi/4, phi_up = 3*np.pi/8, Lambda = self.Lambda, sharpness = self.sharpness)
+        add_poles_low_pos, add_poles_low_neg = _explicit_poles(phi_low = np.pi/8, phi_up = np.pi/4, Lambda = self.Lambda, sharpness = self.sharpness)
+
+        #positive frequencies
+        def upper_residues_pos(w, sharpness, h, Lambda, phi):
+            return (1/sharpness) * 1/(1+np.exp(-sharpness * (w + Lambda)))* np.exp(2.j * np.pi* np.log(w * np.exp(-1.j * phi))/h)/(1-np.exp(2.j * np.pi  * np.log(w * np.exp(-1.j * phi))/h))
+        def bottom_residues_pos(w, sharpness, h, Lambda, phi):
+            return -(1/sharpness) * 1/(1+np.exp(-sharpness * (w + Lambda)))* np.exp(-2.j * np.pi * np.log(w * np.exp(-1.j * phi))/h)/(1-np.exp(-2.j * np.pi  * np.log(w * np.exp(-1.j * phi))/h))
+        #negative frequencies
+        def upper_residues_neg(w, sharpness, h, Lambda, phi):
+            return -(1/sharpness) * 1/(1+np.exp(sharpness * (w - Lambda)))* np.exp(-2.j * np.pi  * np.log(-w * np.exp(1.j * phi))/h)/(1-np.exp(-2.j * np.pi  * np.log(-w * np.exp(1.j * phi))/h))
+        def bottom_residues_neg(w, sharpness, h, Lambda, phi):
+            return (1/sharpness) * 1/(1+np.exp(+sharpness * (w - Lambda)))* np.exp(2.j * np.pi  * np.log(-w * np.exp(1.j * phi))/h)/(1-np.exp(2.j * np.pi  * np.log(-w * np.exp(1.j * phi))/h))
+
+        upper_res_pos_arr = upper_residues_pos(add_poles_up_pos, self.sharpness, h, self.Lambda, phi = phi)
+        lower_res_pos_arr = bottom_residues_pos(add_poles_low_pos, self.sharpness, h, self.Lambda, phi = phi)
+        upper_res_neg_arr = upper_residues_neg(add_poles_up_neg, self.sharpness, h, self.Lambda, phi = phi)
+        lower_res_neg_arr = bottom_residues_neg(add_poles_low_neg, self.sharpness, h, self.Lambda, phi = phi)
+
+        #concatenate poles and residues
+        poles = np.concatenate((add_poles_up_pos, add_poles_low_pos, add_poles_up_neg, add_poles_low_neg))
+        residues = np.concatenate((upper_res_pos_arr, lower_res_pos_arr, upper_res_neg_arr, lower_res_neg_arr))
 
         return poles, residues
 
